b64e3118dc3bb93e6e9521bb31c06ebb236cc9d2
Lemlak <j.brukner@seznam.cz>
2023-07-29 10:41:18 +0200

    Updating run script

--- sg_script.sh
+++ sg_script.sh
@@ -2,17 +2,17 @@
 #!/bin/bash
 #
 #$ -S /bin/bash
-#$ -N TDNN_resemblyzer_toggle_wgan_bigger_tdnn
-#$ -o /mnt/matylda6/ibrukner/code/Bolaji_idea/new_exp/2TDNN_resemblyzer_bolaji_toggle_wgan/exp_bigger_tdnn_dropout/out.txt
-#$ -e /mnt/matylda6/ibrukner/code/Bolaji_idea/new_exp/2TDNN_resemblyzer_bolaji_toggle_wgan/exp_bigger_tdnn_dropout/err.txt
-#$ -l ram_free=4G,mem_free=4G,gpu=1,gpu_ram=16G,cpu=16
+#$ -N AutoVC_TDNN_32x8_adaptive_toggle_wgan_aug_crit_1
+#$ -o /mnt/matylda6/ibrukner/code/AutoVC_TDNN/adaptive_toggle_wgan_aug/AutoVC_TDNN_32x8_adaptive_toggle_wgan_aug_crit_1_1/out.txt
+#$ -e /mnt/matylda6/ibrukner/code/AutoVC_TDNN/adaptive_toggle_wgan_aug/AutoVC_TDNN_32x8_adaptive_toggle_wgan_aug_crit_1_1/err.txt
+#$ -l ram_free=8G,mem_free=8G,gpu=1,gpu_ram=8G,cpu=32
 #$ -q long.q@@speech-gpu
 #
 
 ulimit -t 720000
-script_root=/mnt/matylda6/ibrukner/code/Bolaji_idea
-dest=/mnt/matylda6/ibrukner/code/Bolaji_idea/new_exp/2TDNN_resemblyzer_bolaji_toggle_wgan/exp_bigger_tdnn_dropout
+
+dest=/mnt/matylda6/ibrukner/code/AutoVC_TDNN/adaptive_toggle_wgan_aug/AutoVC_TDNN_32x8_adaptive_toggle_wgan_aug_crit_1_1
 script=$dest/main.py
 dataset=/mnt/matylda6/ibrukner/datasets/VCTK_one_shot
 output=$dest
-python $script --output $output --conf $output --dataset $dataset --cont
+python $script --output $output --conf $output --dataset $dataset #--cont
