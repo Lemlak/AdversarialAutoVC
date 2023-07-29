d671da0dbc29cacd88f96d241dbfd398472056ee
Lemlak <j.brukner@seznam.cz>
2023-07-29 10:43:37 +0200

    Clean up sg_script.sh from paths

--- sg_script.sh
+++ sg_script.sh
@@ -3,16 +3,14 @@
 #
 #$ -S /bin/bash
 #$ -N AutoVC_TDNN_32x8_adaptive_toggle_wgan_aug_crit_1
-#$ -o /mnt/matylda6/ibrukner/code/AutoVC_TDNN/adaptive_toggle_wgan_aug/AutoVC_TDNN_32x8_adaptive_toggle_wgan_aug_crit_1_1/out.txt
-#$ -e /mnt/matylda6/ibrukner/code/AutoVC_TDNN/adaptive_toggle_wgan_aug/AutoVC_TDNN_32x8_adaptive_toggle_wgan_aug_crit_1_1/err.txt
+#$ -o # out path for sge
+#$ -e # err path for sge
 #$ -l ram_free=8G,mem_free=8G,gpu=1,gpu_ram=8G,cpu=32
-#$ -q long.q@@speech-gpu
+#$ -q # queue for sge
 #
 
-ulimit -t 720000
-
-dest=/mnt/matylda6/ibrukner/code/AutoVC_TDNN/adaptive_toggle_wgan_aug/AutoVC_TDNN_32x8_adaptive_toggle_wgan_aug_crit_1_1
-script=$dest/main.py
-dataset=/mnt/matylda6/ibrukner/datasets/VCTK_one_shot
-output=$dest
-python $script --output $output --conf $output --dataset $dataset #--cont
+script=main.py
+dataset=#dataset path
+output=#output path
+# config name is fixed, set directory of experiment
+python $script --output $output --conf ./ --dataset $dataset #--cont
